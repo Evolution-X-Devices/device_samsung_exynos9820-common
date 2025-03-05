LineageOS/android_device_samsung_exynos9820-common

exynos9820-common: import audio namespace and set soong parameters
Change-Id: I2a0e6b8baeb2ce340ffc1ffafbe5fb58c2e234d1

exynos9825-r.mk
@@ -19,6 +19,10 @@ COMMON_PATH := device/samsung/exynos9820-common
 # Inherit common configuration
 $(call inherit-product, $(COMMON_PATH)/common.mk)
 
+# audio
+PRODUCT_PACKAGES += \
+    audio.primary.universal9825
+
 # gralloc
 PRODUCT_PACKAGES += \
     gralloc.universal9825

Evolution-X-Devices/device_samsung_exynos9820-common

exynos9825-r.mk
@@ -19,6 +19,10 @@ COMMON_PATH := device/samsung/exynos9820-common
 # Inherit common configuration
 $(call inherit-product, $(COMMON_PATH)/common.mk)
 
+# Audio
+PRODUCT_PACKAGES += \
+    audio.primary.universal9825_r
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.universal9825_r